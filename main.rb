decac333c538d3021450babdb5135bbfd84f3640
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-03-04 22:11:43 +0000

    example of predictive recursive descent parser

---
+++ main.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'slim'
+require 'sass'
+require 'v8'
+require 'coffee-script'
+
+before do
+  set_title
+end
+
+helpers do
+  def css(*stylesheets)
+    stylesheets.map do |stylesheet| 
+      %Q{
+        <link href="/#{stylesheet}.css" 
+              media="screen, projection" 
+              rel="stylesheet" 
+        />
+      }
+    end.join
+  end
+  
+  def current?(path='/')
+    (request.path==path || request.path==path+'/') ? "current" : nil
+  end
+  
+  def set_title
+    @title ||= "Predictive Recursive Descent Parser"
+  end
+end
+
+get('/styles.css'){ scss :styles }
+get('/javascripts/main.js'){ coffee :main }
+
+get '/' do
+  slim :home
+end
+
+not_found do
+  slim :not_found
+end
+
